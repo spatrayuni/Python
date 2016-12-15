a979198a0e821e527feaeb45db29599d0c72190d
Santhosh Deepu Patrayuni <spatrayuni@gmail.com>
2016-12-15 22:13:47 +0530

    Uploading Boolean sample script

---
+++ Learning/Boolean_sample_1.py
@@ -0,0 +1,7 @@
+"""This program is for giving a sample on Boolean """
+
+#Main Function
+if __name__ == "__main__":
+	x = True
+	y = False
+	print "%s and %s are Boolean variables for which the values should be either True or False and the first letter should always be Capital letter" %(x,y)
