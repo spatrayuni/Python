42429338ac9abf7f19a12d595244c70506c0f3f9
Santhosh Deepu Patrayuni <spatrayuni@gmail.com>
2016-12-25 17:18:37 +0530

    uploading Strings_sample_1 script

---
+++ Learning/Strings_sample_1.py
@@ -0,0 +1,12 @@
+"""This example is all about string operations
+1. How to use String variables to concatination
+"""
+import sys
+
+if __name__ == "__main__":
+	first_arg = sys.argv[1]
+	second_arg = sys.argv[2]
+
+	concatinated_string = first_arg + second_arg
+
+	print concatinated_string
